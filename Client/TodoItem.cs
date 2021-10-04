0c113666d1bd6108b203bb1f8636f625df1ced1c
Jacob Edge <jedge8616@eagle.fgcu.edu>
2021-10-04 18:51:22 -0400

    Added a Todo list as well as icons to each page within the application

---
+++ Client/TodoItem.cs
@@ -0,0 +1,5 @@
+public class TodoItem
+{
+    public string Title { get; set; } //Creates a getter and setter to get the title of the todo item.
+    public bool IsDone { get; set; } //Creates a getter and setter to see if is done is true or flase.
+}
